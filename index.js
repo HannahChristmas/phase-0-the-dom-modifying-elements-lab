dec7b39edb33b52ee33a06b473d3fab9f979d25b
Hannah Christmann <hc109909@gmail.com>
2022-08-02 11:04:47 -0400

    commit tree 6a2acc99d46e1bf7500caaaa84729f53ad84cb40

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Hannah is the champion";
+
+
+// it("has a 'newHeader' variable that points to an <h1> node, 'h1#victory'", () => {
+//     expect(
+//       newHeader.nodeName,
+//       "Make sure you create an <h1> with id 'victory'"
+//     ).eql("H1");
+//   });
